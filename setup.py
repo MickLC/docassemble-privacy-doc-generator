0d82109c7c9a578ac819e624980b0b55bceaf780
Mickey <github@spamtacular.com>
2026-07-08 14:53:40 -0500

    Version bump

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='docassemble-privacy-doc-generator',
-    version='0.1.0',
+    version='0.1.1',
     description='A Docassemble package for generating GDPR-compliant privacy policy documents',
     long_description=open('README.md').read(),
     long_description_content_type='text/markdown',
